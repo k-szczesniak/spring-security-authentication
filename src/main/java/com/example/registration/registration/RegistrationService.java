8c4a52e7613e88a241f387c26c49372ecc6193ac
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2021-05-07 00:26:51 +0200

    Registration new user with db query added

--- src/main/java/com/example/registration/registration/RegistrationService.java
+++ src/main/java/com/example/registration/registration/RegistrationService.java
@@ -1,5 +1,8 @@
 package com.example.registration.registration;
 
+import com.example.registration.appuser.AppUser;
+import com.example.registration.appuser.AppUserRole;
+import com.example.registration.appuser.AppUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +10,7 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class RegistrationService {
 
+    private final AppUserService appUserService;
     private EmailValidator emailValidator;
 
     public String register(RegistrationRequest request) {
@@ -14,6 +18,6 @@ public class RegistrationService {
         if (!isValidEmail) {
             throw new IllegalStateException("Email not valid");
         }
-        return "it works";
+        return appUserService.singUpUser(new AppUser(request.getFirstName(), request.getLastName(), request.getEmail(), request.getPassword(), AppUserRole.USER));
     }
 }
