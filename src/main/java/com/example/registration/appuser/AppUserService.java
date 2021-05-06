bb08103aa8931632cf164ecbe586ed0e8cae0806
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2021-05-07 01:37:01 +0200

    Confirmation of account with token implemented

--- src/main/java/com/example/registration/appuser/AppUserService.java
+++ src/main/java/com/example/registration/appuser/AppUserService.java
@@ -1,11 +1,16 @@
 package com.example.registration.appuser;
 
+import com.example.registration.registration.token.ConfirmationToken;
+import com.example.registration.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -14,6 +19,7 @@ public class AppUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_MSG = "user with email %s not found";
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -25,7 +31,7 @@ public class AppUserService implements UserDetailsService {
         boolean userExists = appUserRepository
                 .findByEmail(appUser.getEmail())
                 .isPresent();
-        if(userExists) {
+        if (userExists) {
             throw new IllegalStateException(String.format("Email: '%s' already taken", appUser.getEmail()));
         }
 
@@ -34,7 +40,15 @@ public class AppUserService implements UserDetailsService {
 
         appUserRepository.save(appUser);
 
-//        TODO: Send confirmation token
-        return "it works";
+        String token = UUID.randomUUID().toString();
+        ConfirmationToken confirmationToken = new ConfirmationToken(token, LocalDateTime.now(), LocalDateTime.now().plusMinutes(15), appUser);
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+//        TODO: Send email
+        return token;
+    }
+
+    public int enableAppUser(String email) {
+        return appUserRepository.enableAppUser(email);
     }
 }
--- src/main/java/com/example/registration/registration/RegistrationService.java
+++ src/main/java/com/example/registration/registration/RegistrationService.java
@@ -3,15 +3,21 @@ package com.example.registration.registration;
 import com.example.registration.appuser.AppUser;
 import com.example.registration.appuser.AppUserRole;
 import com.example.registration.appuser.AppUserService;
+import com.example.registration.registration.token.ConfirmationToken;
+import com.example.registration.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
 public class RegistrationService {
 
     private final AppUserService appUserService;
-    private EmailValidator emailValidator;
+    private final EmailValidator emailValidator;
+    private final ConfirmationTokenService confirmationTokenService;
 
     public String register(RegistrationRequest request) {
         boolean isValidEmail = emailValidator.test(request.getEmail());
@@ -19,5 +25,24 @@ public class RegistrationService {
             throw new IllegalStateException("Email not valid");
         }
         return appUserService.singUpUser(new AppUser(request.getFirstName(), request.getLastName(), request.getEmail(), request.getPassword(), AppUserRole.USER));
+    }
+
+    @Transactional
+    public String confirmToken(String token) {
+        ConfirmationToken confirmationToken = confirmationTokenService.getToken(token).orElseThrow(()-> new IllegalStateException("token not found"));
+
+        if(confirmationToken.getConfirmedAt() != null) {
+            throw new IllegalStateException("email already confirmed");
+        }
+
+        LocalDateTime expiredAt = confirmationToken.getExpiresAt();
+
+        if (expiredAt.isBefore(LocalDateTime.now())) {
+            throw new IllegalStateException("token expired");
+        }
+
+        confirmationTokenService.setConfirmedAt(token);
+        appUserService.enableAppUser(confirmationToken.getAppUser().getEmail());
+        return "confirmed";
     }
 }
