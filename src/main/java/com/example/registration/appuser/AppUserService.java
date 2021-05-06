d08d42cc3c7b44a42ca3d22564c05c787650f3ad
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2021-05-06 21:44:12 +0200

    application.yml database configuration, AppUser class implementation

---
+++ src/main/java/com/example/registration/appuser/AppUserService.java
@@ -0,0 +1,12 @@
+package com.example.registration.appuser;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AppUserService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
