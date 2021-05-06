16f74376687728a800a593583c7ddada86d2472e
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2021-05-06 23:46:59 +0200

    WebSecurity configuration

--- src/main/java/com/example/registration/registration/RegistrationService.java
+++ src/main/java/com/example/registration/registration/RegistrationService.java
@@ -1,7 +1,19 @@
 package com.example.registration.registration;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email not valid");
+        }
+        return "it works";
     }
 }
