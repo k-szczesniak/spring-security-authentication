6c628e31861fa4cc8724529cf9282a88327fdb13
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2021-11-12 12:06:40 +0100

    application-test.yml added to change datasource for tests

--- src/test/java/com/example/registration/RegistrationApplicationTests.java
+++ src/test/java/com/example/registration/RegistrationApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.registration;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RegistrationApplicationTests {
 
 	@Test
